832d28b28f74765c20c4f92f3ca1673cb1605808
Pavan0445 <95559407+Pavan0445@users.noreply.github.com>
2023-05-19 16:08:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2-instance" {
   instance_type = var.ec2type
   tenancy  = var.ec2tenancy
   tags = var.ec2tags
+  vpc_id = var.ec2v_vpc
   subnet_id = var.ec2subnetid
   vpc_security_group_ids = var.ec2sggroups
   
